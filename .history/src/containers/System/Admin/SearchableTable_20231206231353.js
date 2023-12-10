7e456c67184eaa5c7213f3a98181d3b5f5f5c80f
phucbao2k <baophucta2k@gmail.com>
2023-12-11 02:03:50 +0700

    fd

---
+++ .history/src/containers/System/Admin/SearchableTable_20231206231353.js
@@ -0,0 +1,43 @@
+// SearchableTable.js
+import React, { useState } from 'react';
+import axios from 'axios';
+import './SearchableTable.scss';
+const SearchableTable = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSearch = () => {
+        axios.get(`/search?searchTerm=${searchTerm}`)
+            .then(response => setSearchResults(response.data))
+            .catch(error => console.error(error));
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Enter keyword..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+
+            <table>
+                <thead>
+                    <tr>
+                        {/* Add table headers based on your data structure */}
+                    </tr>
+                </thead>
+                <tbody>
+                    {searchResults.map(result => (
+                        <tr key={result.id}>
+                            {/* Display data in table rows */}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default SearchableTable;
---
+++ .history/src/containers/System/Admin/SearchableTable_20231206232525.js
@@ -0,0 +1,43 @@
+import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './SearchableTable.scss';
+const SearchableTable = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSearch = () => {
+        axios.get(`/search?searchTerm=${searchTerm}`)
+            .then(response => setSearchResults(response.data))
+            .catch(error => console.error(error));
+    };
+
+    return (
+        <div className="searchable-table-container">
+            <input
+                type="text"
+                placeholder="Enter keyword..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+
+            <table>
+                <thead>
+                    <tr>
+                        {/* Add table headers based on your data structure */}
+                    </tr>
+                </thead>
+                <tbody>
+                    {searchResults.map(result => (
+                        <tr key={result.id}>
+                            {<td>{result.id}</td>}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default connect()(ManagePatients);
